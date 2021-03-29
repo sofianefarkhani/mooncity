e9d2405cbf1064e3956939b6e0a516fe70a284ac
PAYET Guillaume <tiguillaume@orange.fr>
2021-03-29 13:25:15 +0200

    La fusée dispawn au bout d'une certaine hauteur
    J'ai modifié le script rocketLaunch pour qu'elle disparaisse à une certaine hauteur.

--- Assets/Véhicules/Fusée/rocketlaunch.cs
+++ Assets/Véhicules/Fusée/rocketlaunch.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class rocketlaunch : MonoBehaviour
 {
-
+    public float dist;
     public ParticleSystem ps1;
     public ParticleSystem ps2;
     public Light l;
-
-    bool trig = false;
+    
+    bool trig = true;
     float gamma = 0.4f;
     float speed = 0 ;
     // Start is called before the first frame update
@@ -17,12 +18,13 @@ public class rocketlaunch : MonoBehaviour
     {
        
         //GameObject rocket = new GameObject();
+        print("Fusée : "+gameObject.ToString());
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+        
 
         if(trig == true )
         {
@@ -33,7 +35,12 @@ public class rocketlaunch : MonoBehaviour
            
             
             transform.position += new Vector3(0, Time.deltaTime * speed, 0);
-            
+            if (transform.position.y >= dist )
+            {
+                trig = false;
+                Destroy(gameObject);
+                
+            }
         }
     }
 
