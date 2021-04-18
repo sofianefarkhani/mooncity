d1c06c2eff88412603898dd6c2287dcffeda79f3
PAYET Guillaume <tiguillaume@orange.fr>
2021-04-18 14:44:55 +0200

    Modification de la façon dont son détecter les personnes

--- Assets/MultiPlayer/Scripts/ElevatorDetection.cs
+++ Assets/MultiPlayer/Scripts/ElevatorDetection.cs
@@ -7,8 +7,6 @@ public class ElevatorDetection : MonoBehaviour
     private float _timeBeforeElevate;
     [SerializeField]
     private int _numberOfPersonIn;
-    [SerializeField]
-    private bool _isInOrOut;//True = il est à l'intérieur de l'ascenseur, False = il est à l'extérieur de l'ascenseur(par défaut)
     // Start is called before the first frame update
     private void Start()
     {
@@ -18,18 +16,18 @@ public class ElevatorDetection : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        /**********Un simple timer qui se décrémente que s'il est à l'intérieur et qu'il est au moins une personne dans l'ascenseur**********/
-        if (!_isInOrOut || _numberOfPersonIn <= 0) return;
-        if (_timeBeforeElevate > 0)
+        /********** Un simple timer qui se décrémente que s'il y a au moin une personne à l'intérieur de l'ascenseur **********/
+        if(_numberOfPersonIn<=0) return;
+        if (_timeBeforeElevate > 0 )
         {
             _timeBeforeElevate -= Time.smoothDeltaTime;
             Debug.Log("Timer :"+_timeBeforeElevate);
         }
         else
         {
-            /**********Le timer est arriver à zéro donc on TP les personnes qui sont à l'intérieur de l'ascenseur**********/
+            /********** Le timer est arriver à zéro donc on TP les personnes qui sont à l'intérieur de l'ascenseur **********/
             Debug.Log("We are ready to go !");
-            _isInOrOut = false;
+            _numberOfPersonIn = 0;
             _timeBeforeElevate = DefaultTime;
             SwitchingRoom.SwitchRoom();
 
@@ -38,23 +36,16 @@ public class ElevatorDetection : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (_isInOrOut) return;//Si il est déjà à l'intérieur, on ne fait rien.
-        /***********S'il est pas à l'intérieur de base et qu'il entre dans l'ascenseur************/
-        _timeBeforeElevate = DefaultTime;//On reset le timer pour que tout le monde soit synchroniser pour la téléportation.
-        _isInOrOut = true;//On indique qu'il est à l'intérieur de l'ascenseur.
+        
+        if(!other.GetType().IsEquivalentTo(typeof(CharacterController))) return;
         _numberOfPersonIn++;
-        Debug.Log("A person has entered the elevator ! Number of person inside :"+_numberOfPersonIn);
-
+        _timeBeforeElevate = DefaultTime;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if(!_isInOrOut)return;//Si il est déjà à l'extérieur de l'ascenseur, on fait rien.
-        /**********S'il est pas à l'extérieur(càd qu'il est à l'intérieur de l'ascenseur) et qu'il sort de l'ascenseur**********/
-        _isInOrOut = false;//On indique qu'il est à l'extérieur;(càd qu'il est sortie de l'ascenseur)
-        if (_numberOfPersonIn < 0) return;
-        _numberOfPersonIn--;//On mets à jour le nombre de personne à l'intérieur de l'ascenseur
-        Debug.Log("One person has left the elevator ! Number of person left inside : "+_numberOfPersonIn);
+        if(!other.GetType().IsEquivalentTo(typeof(CharacterController))) return;
+        _numberOfPersonIn--;
 
     }
 }
