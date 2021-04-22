d63b3141a04f101025fe21bf8c394a49716998ee
TheOrtech <aurelienchevallereau@icloud.com>
2021-04-22 16:08:38 +0200

    Circuit fonctionnel

--- Assets/MultiPlayer/Scripts/CharacterControllerVR/VRController.cs
+++ Assets/MultiPlayer/Scripts/CharacterControllerVR/VRController.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
 using Photon.Pun;
+
 
 public class VRController : MonoBehaviourPun
 {
