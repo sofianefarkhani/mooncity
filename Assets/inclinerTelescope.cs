506db353e231cdd76ccbd2dbf0d8a09b2c7be210
esteban aragon <aragone@3il.fr>
2021-04-24 12:15:43 +0200

    Scripts de manipulation du téléscope
    Scripts testés et fonctionnels en singleplayer.
    Multi a tester.
    Pas de boutons implémentés pour une accession par le joueur.
    => accéder par la main toute puissante de l'éditeur unity.

---
+++ Assets/inclinerTelescope.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inclinerTelescope : MonoBehaviour
+{
+    GameObject upperPart;
+    bool pointUpwards;
+    bool pointDownwards;
+
+    float higherInclination;
+    float lowerInclination;
+    float midPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        upperPart = this.gameObject;
+        pointDownwards = false;
+        pointUpwards = false;
+
+        higherInclination = 335;
+        lowerInclination = 50;
+        midPoint = 180;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = upperPart.transform.rotation.eulerAngles.x;
+
+        float speed = 10f;
+        float absoluteAngleBonus = Time.deltaTime * speed;
+
+        //butées limitant le mouvement du telescope
+        if (angle > midPoint && angle < higherInclination)
+        {
+            pointUpwards = false;
+            pointDownwards = true;
+        }
+        if (angle < midPoint && angle > lowerInclination)
+        {
+            pointUpwards = true;
+            pointDownwards = false;
+        }
+
+
+        //mouvement
+        if (pointUpwards)
+        {
+            upperPart.transform.Rotate(-absoluteAngleBonus, 0, 0);
+        }
+        else if (pointDownwards)
+        {
+            upperPart.transform.Rotate(absoluteAngleBonus, 0, 0);
+        }
+    }
+
+    //GETTERS SETTERS
+    void setPointUpwards(bool turnUp)
+    {
+        this.pointUpwards = turnUp;
+    }
+
+    void setSenseOfRotationToTrigo(bool turnDown)
+    {
+        this.pointDownwards = turnDown;
+    }
+}
---
+++ Assets/tounerTelescope.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tounerTelescope : MonoBehaviour
+{
+
+    public bool rotating;
+    public bool trigonometricRotation;
+    float rotationSpeed;
+
+    GameObject telescope;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        telescope = this.gameObject;
+        rotating = false;
+        trigonometricRotation = true;
+        rotationSpeed = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = telescope.transform.rotation.eulerAngles.y;
+        float absoluteAngleBonus = Time.deltaTime * rotationSpeed;
+
+
+        if (trigonometricRotation)
+        {
+            telescope.transform.Rotate(0, -absoluteAngleBonus, 0);
+        }
+        else
+        {
+            telescope.transform.Rotate(0, absoluteAngleBonus, 0);
+        }
+    }
+
+    //GETTERS SETTERS
+    void setRotating(bool rotates)
+    {
+        this.rotating = rotates;
+    }
+
+    void setSenseOfRotationToTrigo(bool trigoRotation)
+    {
+        this.trigonometricRotation = trigoRotation;
+    }
+}
